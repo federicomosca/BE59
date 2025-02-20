d5869af1e5d4692d7bd8e01afa769efd2085f79e
dogs <federicomosca@pm.me>
2025-02-21 00:11:26 +0100

    Initial commit - Spring Boot project setup

---
+++ src/main/java/it/dogs/fivenine/FivenineApplication.java
@@ -0,0 +1,13 @@
+package it.dogs.fivenine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FivenineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FivenineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/dogs/fivenine/FivenineApplicationTests.java
@@ -0,0 +1,13 @@
+package it.dogs.fivenine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FivenineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
