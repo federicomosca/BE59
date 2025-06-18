036635963bbcd684129c282a078a4a927beecba4
federicomosca <federicomosca01@outlook.it>
2025-06-18 22:44:57 +0200

    x

--- src/main/java/it/dogs/fivenine/controller/CollectionController.java
+++ src/main/java/it/dogs/fivenine/controller/CollectionController.java
@@ -12,14 +12,4 @@ public class CollectionController {
 
     @Autowired
     private CollectionService collectionService;
-
-    @PostMapping
-    public Collection createCollection(@RequestBody Collection collection) {
-        return collectionService.createCollection(collection);
-    }
-
-    @GetMapping("/{id}")
-    public Collection getCollectionById(@PathVariable Long id) {
-        return collectionService.getCollectionById(id);
-    }
 }
